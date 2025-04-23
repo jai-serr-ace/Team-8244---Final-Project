04d7ee508d418e66f798b63caa32447c732b21f2
Kobesowner712 <dougiemcd@cox.net>
2025-04-23 15:46:05 -0700

    4/23 Work

--- gui.py
+++ gui.py
@@ -1,40 +1,61 @@
 import sys
 from PySide6.QtWidgets import (QWidget, QApplication, QVBoxLayout, QLabel, QHBoxLayout, QPushButton, QComboBox)
-from PySide6.QtCore import Slot, QObject
+from PySide6.QtCore import Slot, QObject, Qt
 from PySide6.QtGui import QKeyEvent
-from __feature__ import snake_case, true_property
+from __feature__ import snake_case
 
-my_app = QApplication([])
+# my_app = QApplication([])
 
 
 class GUI(QWidget):
     def __init__(self):
         super().__init__()
         self.window_title = 'Final Project GUI Test'
-        hbox = QHBoxLayout()
-        my_btn1 = QPushButton('A')
-        my_btn2 = QPushButton('B')
-        my_btn3 = QPushButton('C')
-        my_btn4 = QPushButton('D')
-        my_btn5 = QPushButton('E')
-        my_btn6 = QPushButton('F')
-        my_btn7 = QPushButton('G')
-        my_btn8 = QPushButton('A')
-        hbox.add_widget(my_btn1)
-        hbox.add_widget(my_btn2)
-        hbox.add_widget(my_btn3)
-        hbox.add_widget(my_btn4)
-        hbox.add_widget(my_btn5)
-        hbox.add_widget(my_btn6)
-        hbox.add_widget(my_btn7)
-        hbox.add_widget(my_btn8)
+        hbox = QHBoxLayout(self)
+        self.label = QLabel('Hi')
+        hbox.add_widget(self.label)
         self.set_layout(hbox)
+    # def keyPressEvent(self, event: QKeyEvent):
+    #     if event.key() == Qt.Key.Key_A:
+    #         self.label.setText("A was pressed")
+    #     elif event.key() == Qt.Key.Key_B:
+    #         self.label.setText("B was pressed")
+    #     elif event.key() == Qt.Key.Key_Escape:
+    #         self.close()
+    #     else:
+    #         self.label.setText("Some other key was pressed")
+
+# app = GUI()
+# app.show()
+# sys.exit(my_app.exec())
+
+
+
+#Online Example (Working)
+# from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+# from PySide6.QtGui import QKeyEvent
+# from PySide6.QtCore import Qt
+
+# class MyWidget(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         self.label = QLabel("Press a key")
+#         layout = QVBoxLayout(self)
+#         layout.addWidget(self.label)
+#         self.setLayout(layout)
+
     def keyPressEvent(self, event: QKeyEvent):
-        key = event.key()
-        print(f"Found key {key}!")
+        if event.key() == Qt.Key.Key_A:
+            self.label.setText("A was pressed")
+        elif event.key() == Qt.Key.Key_B:
+            self.label.setText("B was pressed")
+        elif event.key() == Qt.Key.Key_Escape:
+            self.close()
+        else:
+            self.label.setText("Some other key was pressed")
 
-
-app = GUI()
-app.show()
-
-sys.exit(my_app.exec())
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = GUI()
+    widget.show()
+    app.exec()
