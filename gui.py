3ea3b848847de7fdc157a00d805d738b0a4b73b3
Kobesowner712 <dougiemcd@cox.net>
2025-04-27 23:56:20 -0700

    Weekend Progress, GUI Test Functional

--- gui.py
+++ gui.py
@@ -1,15 +1,36 @@
 import sys
 from PySide6.QtWidgets import (QWidget, QApplication, QVBoxLayout, QLabel, QHBoxLayout, QPushButton, QComboBox)
-from PySide6.QtCore import Slot
-from __feature__ import snake_case, true_property
+from PySide6.QtCore import Slot, QObject, Qt, QUrl
+from PySide6.QtMultimedia import QSoundEffect
+from PySide6.QtGui import QKeyEvent
+import numpy as np
+import sounddevice as sd
+from playsound import playsound
 
-my_app = QApplication([])
+
+
+def generate_sine_wave(frequency, duration, sample_rate):
+    """Generates a sine wave at a given frequency for a specified duration."""
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+    amplitude = 50 * np.sin(2 * np.pi * frequency * t)
+    #the 2 on amplitude shifts the pitch; so dont move. The 5 value changes the strength of the
+    #wave of the note, and as it right now it sounds like 8-bit
+    return amplitude
+
+def notePlayer(my_note, length):
+    duration_seconds = length
+    sample_rate = 48000
+    sine_wave = generate_sine_wave(my_note, duration_seconds, sample_rate)
+    sd.play(sine_wave, sample_rate)
+    sd.save()
+    sd.wait()
+
 
 
 class GUI(QWidget):
     def __init__(self):
         super().__init__()
-        self.window_title = 'Final Project GUI Test'
+        self.windowTitle = 'Final Project GUI Test'
         hbox = QHBoxLayout()
         my_btn1 = QPushButton('A')
         my_btn2 = QPushButton('B')
@@ -19,18 +40,46 @@ class GUI(QWidget):
         my_btn6 = QPushButton('F')
         my_btn7 = QPushButton('G')
         my_btn8 = QPushButton('A')
-        hbox.add_widget(my_btn1)
-        hbox.add_widget(my_btn2)
-        hbox.add_widget(my_btn3)
-        hbox.add_widget(my_btn4)
-        hbox.add_widget(my_btn5)
-        hbox.add_widget(my_btn6)
-        hbox.add_widget(my_btn7)
-        hbox.add_widget(my_btn8)
-        self.set_layout(hbox)
+        hbox.addWidget(my_btn1)
+        hbox.addWidget(my_btn2)
+        hbox.addWidget(my_btn3)
+        hbox.addWidget(my_btn4)
+        hbox.addWidget(my_btn5)
+        hbox.addWidget(my_btn6)
+        hbox.addWidget(my_btn7)
+        hbox.addWidget(my_btn8)
+        self.setLayout(hbox)
+    def keyPressEvent(self, event: QKeyEvent):
+        if event.key() == Qt.Key.Key_A:
+            # notePlayer(440, 2)
+            global effect
+            effect = QSoundEffect()
+            effect.setSource(QUrl.fromLocalFile("C:/Users/dougi/cst205/Team-8244---Final-Project/testAudios/a4.wav"))
+            # effect.setVolume(2)
+            effect.play()
+        if event.key() == Qt.Key.Key_D:
+            # notePlayer(830.61, 2)
+            global effect2
+            effect2 = QSoundEffect()
+            effect2.setSource(QUrl.fromLocalFile("C:/Users/dougi/cst205/Team-8244---Final-Project/testAudios/e5.wav"))
+            # effect.setVolume(2)
+            effect2.play()
+        if event.key() == Qt.Key.Key_S:
+            # notePlayer(554.37, 2)
+            global effect3
+            effect3 = QSoundEffect()
+            effect3.setSource(QUrl.fromLocalFile("C:/Users/dougi/cst205/Team-8244---Final-Project/testAudios/c#.wav"))
+            # effect.setVolume(2)
+            effect3.play()
+        if event.key() == Qt.Key.Key_F:
+            # notePlayer(659.255, 2)
+            global effect4
+            effect4 = QSoundEffect()
+            effect4.setSource(QUrl.fromLocalFile("C:/Users/dougi/cst205/Team-8244---Final-Project/testAudios/ab.wav"))
+            # effect.setVolume(2)
+            effect4.play()
 
-
+my_app = QApplication([])
 app = GUI()
 app.show()
-
 sys.exit(my_app.exec())
